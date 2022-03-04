18c09cb9fbf4787602b25ab756ba51fabbc570b4
silvano.araujo <silvanoeng@hotmail.com>
2022-03-04 19:05:13 -0300

    atulizando arquivos no mac

--- Atividades/src/com/gama/academy/FolhaPagamento/Main.java
+++ Atividades/src/com/gama/academy/FolhaPagamento/Main.java
@@ -1,18 +1,32 @@
 package com.gama.academy.FolhaPagamento;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Funcionario lista[] = new Funcionario[4];
-
-        lista[0] = new Chefe("Silvano", 123, 5124.30f, 250.50f, 12.40f);
-        lista[1] = new Comissionado("Sandro", 456, 4654.45f, 45.50f);
-        lista[2] = new Horista("Sandy", 789, 51.30f, 250);
-        lista[3] = new Empreiteiro("Sibele", 963, 50124.30f);
+        List<Funcionario> lista = Arrays.asList(new Chefe("Silvano", 123, 5124.30f, 250.50f, 12.40f),
+                                            new Comissionado("Sandro", 456, 4654.45f, 45.50f),
+                                            new Horista("Sandy", 789, 51.30f, 250),
+                                            new Empreiteiro("Sibele", 963, 50124.30f));
 
 
-        for (Funcionario f:lista){
+        /*for (Funcionario f:lista){
             System.out.println(f);
             System.out.printf("O valor a receber é R$ %.2f.\n", f.calcularSalario());
-        }
+        }*/
+        System.out.println("Listagem de funcionários em ordem de salário");
+        lista.stream().sorted().forEach(e-> System.out.printf( "%s recebe\tR$ %.2f.\n", e.getNome() , e.calcularSalario()));
+        System.out.println();
+        System.out.println("Listagem de funcionários em ordem alfabética");
+        lista.stream().sorted(Comparator.comparing(p->p.getNome()))
+                        .forEach(e-> System.out.printf( "%s\t", e.getNome()));
+
+        System.out.println("\n");
+        System.out.println("Listagem de funcionários em ordem de cadastro");
+        lista.stream().sorted(Comparator.comparing(p->p.getNumeroRegistro()))
+                .forEach(e-> System.out.printf( "%s\t", e.getNome()));
+
     }
 }
